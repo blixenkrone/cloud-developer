e064900b451d681a90fdad4e3790566f8eded93c
blixenkrone <sblixenkrone@gmail.com>
2020-05-03 18:53:28 +0200

    makefile and compose

--- projects/project3/udagram-api/src/server.ts
+++ projects/project3/udagram-api/src/server.ts
@@ -1,18 +1,19 @@
 import cors from 'cors';
 import express from 'express';
-import {sequelize} from './sequelize';
+import { sequelize } from './sequelize';
 
-import {IndexRouter} from './controllers/v0/index.router';
+import { IndexRouter } from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
-import {config} from './config/config';
-import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
+import { config } from './config/config';
+import { V0_FEED_MODELS, V0_USER_MODELS } from './controllers/v0/model.index';
+import Bluebird from 'bluebird';
 
 
 (async () => {
-  await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
-  await sequelize.sync();
+  sequelize.addModels(V0_FEED_MODELS);
+  sequelize.addModels(V0_USER_MODELS);
+  const b = await sequelize.sync();
 
   const app = express();
   const port = process.env.PORT || 8080;
@@ -32,14 +33,14 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
   app.use('/api/v0/', IndexRouter);
 
   // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
-  } );
+  app.get('/', async (req, res) => {
+    res.send('/api/v0/');
+  });
 
 
   // Start the Server
-  app.listen( port, () => {
-    console.log( `server running ${config.url}` );
-    console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running ${config.url}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
