20c592268c6e9cce73d65939bb38dbab337c51d6
blixenkrone <sblixenkrone@gmail.com>
2020-05-06 11:31:52 +0200

    ngninx

--- projects/project3/api/users-service/src/controllers/v0/users/index.router.ts
+++ projects/project3/api/users-service/src/controllers/v0/users/index.router.ts
@@ -6,7 +6,8 @@ const router: Router = Router();
 router.use('/users', UserRouter);
 
 router.get('/', async (req: Request, res: Response) => {
-    res.send(`V0 feed`);
+    console.log('users feed')
+    res.send(`users v0 feed`);
 });
 
 export const IndexRouter: Router = router;
